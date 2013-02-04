5133a362007003bd511265d966dd4751fe72e991
Chris Cacciatore <chris.cacciatore@gmail.com>
2013-02-04 10:14:29 -0800

    again

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,5 +1,5 @@
 
-NewRelic::Agent.after_fork(:force_reconnect => true) if defined? Unicorn
+::NewRelic::Agent.after_fork(:force_reconnect => true) if defined? Unicorn
 worker_processes 6
 timeout 30
 preload_app true
