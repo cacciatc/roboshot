a951c6569579bce9edb12e0827871858e14160cf
Chris Cacciatore <chris.cacciatore@gmail.com>
2013-02-04 10:28:02 -0800

    again

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,8 +1,6 @@
-configure :production do
-  require 'newrelic_rpm'
-end
+require 'newrelic_rpm'
 
-::NewRelic::Agent.after_fork(:force_reconnect => true) if defined? Unicorn
+NewRelic::Agent.after_fork(:force_reconnect => true) if defined? Unicorn
 worker_processes 6
 timeout 30
 preload_app true
